0762bbaa72589c2d8c9f7dcf25fcf1c0cb71d5ff
Michel065 <Mastergaby065@gmail.com>
2024-12-09 19:35:49 +0100

    maj

--- Carte.java
+++ Carte.java
@@ -5,41 +5,44 @@ import java.util.List;
 import java.util.Random;
 
 public class Carte {
-    private int taille,taille_reel,nbr_max_obstacle_par_case;
-    private double pourcentage_obstacle,ratio_convertion;
+
+    private long taille_origine,taille_reel,nbr_obstacle_totale,longeur_list_obstacle;
+    private int largeur_case,largeur_matrice,nbr_moyen_obstacle_par_case;
+    private double pourcentage_obstacle;
+
     private MatriceCarre<Tuple> carte;
     private List<Obstacle> obstacles;
     
-    public Carte(int taille_reel, double pourcentage_obstacle,int nbr_moyen_obstacle_par_case,int nbr_max_obstacle_par_case) {
-        this.taille_reel=taille_reel;
-        this.taille = (int) Math.ceil(Math.sqrt((taille_reel * taille_reel * pourcentage_obstacle) / nbr_moyen_obstacle_par_case));
-
-        this.nbr_max_obstacle_par_case=nbr_max_obstacle_par_case;
-
+    public Carte(long taille, double pourcentage_obstacle,int nbr_moyen_obstacle_par_case) {
+        this.taille_origine=taille;
+        this.nbr_obstacle_totale=(long) Math.ceil(pourcentage_obstacle*taille_origine*taille_origine);
+        this.largeur_case=(int) Math.ceil(taille_origine/Math.sqrt(nbr_obstacle_totale/nbr_moyen_obstacle_par_case));
+        this.largeur_matrice=(int) Math.ceil(taille_origine/largeur_case);
+        this.nbr_moyen_obstacle_par_case = nbr_moyen_obstacle_par_case;
         this.pourcentage_obstacle = pourcentage_obstacle;
-        ratio_convertion = (double) this.taille_reel / this.taille;
+        this.taille_reel=(long)(largeur_case*largeur_matrice);
+        this.longeur_list_obstacle=largeur_case*largeur_matrice*largeur_matrice;
 
-        carte=new MatriceCarre<Tuple>(taille);
+        carte=new MatriceCarre<Tuple>(largeur_matrice);
+        obstacles = new ArrayList<>((int)longeur_list_obstacle);
 
-        obstacles = new ArrayList<>(nbr_max_obstacle_par_case*taille*taille);
         init_liste();
-
         init_matrice();
         
     }
 
     private void init_liste(){
-        for(int i=0;i<nbr_max_obstacle_par_case*taille*taille;i++){
+        for(int i=0;i<largeur_case*largeur_matrice*largeur_matrice ;i++){
             obstacles.add(null);
         }
     }
 
     private void init_matrice(){
         int indice_dans_liste=0;
-        for(int x=0;x<taille;x++){
-            for(int y=0;y<taille;y++){
+        for(int x=0;x<largeur_matrice;x++){
+            for(int y=0;y<largeur_matrice;y++){
                 carte.set(x,y,new Tuple(indice_dans_liste,indice_dans_liste));
-                indice_dans_liste+=nbr_max_obstacle_par_case;
+                indice_dans_liste+=(largeur_case*largeur_case);
             }
         }
     }
@@ -71,7 +74,7 @@ public class Carte {
     private boolean ajoute_obstacle(int x,int y){
         Tuple couple = carte.get(get_Coordonnees_De_Reel_Vers_Grille(x,y));
         
-        if(couple.y-couple.x<nbr_max_obstacle_par_case && !Obstacle_existe_deja(x,y,couple)){
+        if(!Obstacle_existe_deja(x,y,couple)){
             obstacles.set(couple.y, new Obstacle(x, y));
             couple.y++;
             return true;
@@ -86,11 +89,16 @@ public class Carte {
 
     public void printinfo(){
         System.out.println("____Carte____");
-        System.out.println("taille d'origine:"+taille_reel);
-        System.out.println("taille Mat:"+taille);
-        System.out.println("largeur case:"+ (int)Math.ceil(getLargeurCase()));
-
-        System.out.println("nbr Obstacle par case:"+nbr_max_obstacle_par_case);
+        System.out.println("largeur d'orgine:"+taille_origine);
+        System.out.println("nbr moyen d'obstacle par case de la matrice:"+nbr_moyen_obstacle_par_case);
+        System.out.println("le fameux taux:"+pourcentage_obstacle);
+        System.out.println("nbr d'obstacle calcule:"+nbr_obstacle_totale);
+        System.out.println("largeur d'une case de la matrice:"+largeur_case);
+        System.out.println("largeur de la matrice:"+largeur_matrice);
+        System.out.println("nbr de cases d'origine:"+taille_origine*taille_origine);
+        System.out.println("nouveau nbr de case:"+taille_reel*taille_reel);
+        System.out.println("soit reel>origine ?:"+(taille_origine*taille_origine>=taille_reel*taille_reel));
+        System.out.println("____________");
     }
 
     public void printliste_obstacle(){
@@ -108,8 +116,8 @@ public class Carte {
 
     public void print_nbr_obstacle_par_case_matrice() {
         System.out.println("Nombre d'obstacles par case dans la matrice :");
-        for (int x = 0; x < taille; x++) {
-            for (int y = 0; y < taille; y++) {
+        for (int x = 0; x < largeur_matrice; x++) {
+            for (int y = 0; y < largeur_matrice; y++) {
                 Tuple couple = carte.get(x, y);
                 int nbr_obstacles = couple.y - couple.x; // Nombre d'obstacles dans cette case
                 System.out.print(nbr_obstacles + "\t"); // Affiche avec tabulation
@@ -120,11 +128,11 @@ public class Carte {
     
 
     public double getLargeurCase() {
-        return ratio_convertion=(double) taille_reel / taille;
+        return largeur_case;
     }
 
     public Tuple get_Coordonnees_De_Reel_Vers_Grille(int x, int y) {
-        return new Tuple((int) Math.floor(x / ratio_convertion),(int) Math.floor(y / ratio_convertion));
+        return new Tuple((int) (x / largeur_case),(int) (y / largeur_case));
     }
     
     public void display(int windowWidth, int windowHeight, boolean showGrid) {
@@ -153,13 +161,13 @@ public class Carte {
                 // Dessiner les lignes de la matrice si demandé
                 if (showGrid) {
                     g2d.setColor(Color.BLACK); // Couleur des lignes
-                    for (int i = 0; i <= taille; i++) {
+                    for (int i = 0; i <= largeur_case; i++) {
                         // Lignes verticales
-                        int x = (int) (i * (windowWidth / (double) taille));
+                        int x = (int) (i * (windowWidth / (double) largeur_case));
                         g2d.drawLine(x, 0, x, windowHeight);
     
                         // Lignes horizontales
-                        int y = (int) (i * (windowHeight / (double) taille));
+                        int y = (int) (i * (windowHeight / (double) largeur_case));
                         g2d.drawLine(0, y, windowWidth, y);
                     }
                 }
--- Host.java
+++ Host.java
@@ -3,21 +3,23 @@ import java.util.List;
 public class Host {
     private Carte carte;
     private List<Projectile> projectiles;
+
     Host(){
-        int map = 20; 
-        double pourcentageObstacle = 0.9;
-        int nbrMoyenObstacleParCase = 7;// cmieux si c un carré 
-        int nbrMaxObstacleParCase = 9;
+        long map = 100; 
+        double pourcentageObstacle = 0.15;
+        int nbrMoyenObstacleParCase = 5;
 
         // Initialisation de la carte
-        carte = new Carte(map, pourcentageObstacle, nbrMoyenObstacleParCase, nbrMaxObstacleParCase);
+        carte = new Carte(map, pourcentageObstacle, nbrMoyenObstacleParCase);
 
+        
         // Affichage des informations générales
         carte.printinfo();
 
         // Création de tous les obstacles initiaux
         System.out.println("\nCréation des obstacles...");
         carte.create_all_initial_obstacle();
+        carte.print_nbr_obstacle_par_case_matrice();
         
     }
 

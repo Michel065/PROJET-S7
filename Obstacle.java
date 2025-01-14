7a3f60872a3b6cb1935437b6803ff1238358cc01
Bectaly13 <dodocad13@gmail.com>
2025-01-14 15:56:43 +0100

    Obstacle.java joli

--- Obstacle.java
+++ Obstacle.java
@@ -1,35 +1,35 @@
 public class Obstacle {
     private CoordInt coord;
 
-    public Obstacle(int x,int y){
-        coord=new CoordInt(x,y);
+    public Obstacle(int x, int y) {
+        coord = new CoordInt(x,y);
     }
 
-    public int getx(){
+    public int getx() {
         return coord.x;
     }
 
-    public int gety(){
+    public int gety() {
         return coord.y;
     }
 
-    public void setx(int xx){
-        coord.x=xx;
+    public void setx(int xx) {
+        coord.x = xx;
     }
 
-    public void sety(int yy){
-        coord.y=yy;
+    public void sety(int yy) {
+        coord.y = yy;
     }
 
-    public CoordInt get(){
+    public CoordInt get() {
         return coord;
     }  
 
-    public boolean is_egual(int x, int y){
-        return x==coord.x && y==coord.y;
+    public boolean is_egual(int x, int y) {
+        return x == coord.x && y == coord.y;
     }
 
-    public String stringifi(){
-        return coord.x+":"+coord.y;
+    public String stringifi() {
+        return coord.x + ":" + coord.y;
     }
 }

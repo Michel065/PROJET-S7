f676afdaa72b8f7a3f77f42e9fc7227b10284e0d
Bectaly13 <dodocad13@gmail.com>
2025-01-14 16:00:23 +0100

    Projectile.java joli

--- Projectile.java
+++ Projectile.java
@@ -4,12 +4,12 @@ public class Projectile extends Rond {
     private int degat;
 
     // Constructeur
-    public Projectile(float speed,int life, float radius,int proj_degat, float x, float y, float directionX, float directionY) {
+    public Projectile(float speed, int life, float radius, int proj_degat, float x, float y, float directionX, float directionY) {
         super(life, radius, x, y);
         this.speed = speed;
-        this.degat=proj_degat;
-        this.directionX=directionX;
-        this.directionY=directionY;
+        this.degat = proj_degat;
+        this.directionX = directionX;
+        this.directionY = directionY;
     }
 
     @Override

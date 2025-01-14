44d270831b7d17768b68772c376f6b252eb59a05
Bectaly13 <dodocad13@gmail.com>
2025-01-14 15:50:05 +0100

    CoordX.java joli

--- CoordInt.java
+++ CoordInt.java
@@ -1,13 +1,14 @@
 public class CoordInt {
     public int x;
     public int y;
-    public CoordInt(int val1,int val2){
-        x=val1;
-        y=val2;
+
+    public CoordInt(int val1, int val2){
+        x = val1;
+        y = val2;
     }
 
     public CoordInt(CoordInt tmp){
-        x=tmp.x;
-        y=tmp.y;
+        x = tmp.x;
+        y = tmp.y;
     }
 }

7dd5b4aec7735264762178524b18065137cbf2b4
Bectaly13 <dodocad13@gmail.com>
2025-01-19 00:59:14 +0100

    Joli

--- Carte.java
+++ Carte.java
@@ -114,7 +114,7 @@ public class Carte {
 
     public boolean test_collision_rond_obstacle(CoordFloat coord, float radius) {
         coord_tmp.set(coord);
-        coord_tmp.add((float)0.50-radius);
+        coord_tmp.add((float)0.50 - radius);
 
         CoordInt tmp1 = get_Coordonnees_De_Reel_Vers_Grille((int)coord_tmp.x, (int)coord_tmp.y);
         CoordInt tmp2 = get_Coordonnees_De_Reel_Vers_Grille((int)coord_tmp.x, (int)coord_tmp.y + 1);

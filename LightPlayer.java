ec0f71c888124b1258c6fe41b9f9ee44c610f7c8
Bectaly13 <dodocad13@gmail.com>
2025-01-18 23:05:43 +0100

    Rayon update 0.5 -> 0.45

--- LightPlayer.java
+++ LightPlayer.java
@@ -1,5 +1,5 @@
 public class LightPlayer extends LightRond {
     public LightPlayer(float x, float y, int health_en_pourcentage, int equipe) {
-        super(x, y, (float)0.5, health_en_pourcentage, equipe);
+        super(x, y, (float)0.45, health_en_pourcentage, equipe);
     }
 }

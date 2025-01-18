b666eaa21a35e913cfd5ddce8729bea5569210cf
Bectaly13 <dodocad13@gmail.com>
2025-01-18 23:14:07 +0100

    RAS

--- Player.java
+++ Player.java
@@ -1,7 +1,7 @@
 public class Player extends Rond {
     private double orientation = 0;
     private float max_speed=(float)10, speed_x, speed_y;
-    private int health = 100,max_health = 100;
+    private int health = 100, max_health = 100;
 
     // Infos projectile :
     private float proj_speed = (float)2.5;

83a1c46f9dd1af05ac3a95030c103cb186e3b53c
Bectaly13 <dodocad13@gmail.com>
2025-01-19 01:05:37 +0100

    COLLISIONS OK 0.45

--- Carte.java
+++ Carte.java
@@ -127,8 +127,6 @@ public class Carte {
         CoordInt tmp8 = get_Coordonnees_De_Reel_Vers_Grille((int)coord_tmp.x - 1, (int)coord_tmp.y + 1);
         CoordInt tmp9 = get_Coordonnees_De_Reel_Vers_Grille((int)coord_tmp.x - 1, (int)coord_tmp.y - 1);
 
-        
-
         if (tmp1 == null || tmp2 == null || tmp3 == null || tmp4 == null || tmp5 == null || tmp6 == null || tmp7 == null || tmp8 == null || tmp9 == null) return true;
 
         if(test_collision_rond_obstacle_sur_chunk(coord_tmp, radius, carte.get(tmp1))) return true;

ed4c0363c350d5851e0eb6edc1cfbf78d577b0da
Michel065 <Mastergaby065@gmail.com>
2025-01-21 13:18:38 +0100

    modif

--- ThreadClientToHost.java
+++ ThreadClientToHost.java
@@ -247,7 +247,7 @@ public class ThreadClientToHost extends Thread {
 
     public void respawn_player(){
         if(!player_status){
-            message_transmit="\"put ourplayer new\n\r";
+            message_transmit="put ourplayer new\n\r";
             message_transmit+="put ourplayer invincibilite false\n\r";
             send(message_transmit);
             message_transmit="";
